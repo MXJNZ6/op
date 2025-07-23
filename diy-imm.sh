160133a426ae8ed382dcb37b4eb07266f1037958
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2025-07-23 19:31:40 +0800

    Update diy-imm.sh

--- diy-imm.sh
+++ diy-imm.sh
@@ -1,3 +1,6 @@
 git clone -b js https://github.com/sirpdboy/luci-theme-kucat.git  package/luci-theme-kucat
 git clone https://github.com/sirpdboy/luci-app-advancedplus.git  package/luci-app-advancedplus
 echo "src-git nikki https://github.com/nikkinikki-org/OpenWrt-nikki.git;main" >> "feeds.conf.default"
+
+./scripts/feeds update -a
+./scripts/feeds install -a
