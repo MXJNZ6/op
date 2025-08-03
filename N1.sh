c197bbc210a19df215631ff2ee1fe1564b698e0b
MXJ <53927877+MXJNZ6@users.noreply.github.com>
2025-08-03 18:34:06 +0800

    Update N1.sh

--- N1.sh
+++ N1.sh
@@ -27,13 +27,6 @@ function drop_package(){
 
 rm -rf package/custom; mkdir package/custom
 
-# 添加源
-# sed -i '$a src-git helloworld https://github.com/fw876/helloworld.git' feeds.conf.default
-# sed -i '$a src-git xiaorouji https://github.com/xiaorouji/openwrt-passwall-packages' feeds.conf.default
-# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
-# sed -i '$a src-git nas https://github.com/linkease/nas-packages.git;master' feeds.conf.default
-# sed -i '$a src-git nas_luci https://github.com/linkease/nas-packages-luci.git;main' feeds.conf.default
-
 # 修改openwrt登陆地址,把下面的10.10.10.254修改成你需要的
 sed -i 's/192.168.1.1/10.10.10.254/g' package/base-files/files/bin/config_generate
 
@@ -42,58 +35,21 @@ sed -i "/uci commit system/i\uci set system.@system[0].hostname='Unicorn'" packa
 sed -i "s/hostname='OpenWrt'/hostname='Unicorn'/g" ./package/base-files/files/bin/config_generate
 
 merge_package https://github.com/vernesong/OpenClash OpenClash/luci-app-openclash
-
-#mosdns
-rm -rf feeds/packages/net/mosdns
-rm -rf feeds/luci/applications/luci-app-mosdns
-git clone -b v5-lua https://github.com/sbwml/luci-app-mosdns package/luci-app-mosdns
-merge_package https://github.com/sbwml/luci-app-mosdns luci-app-mosdns/mosdns
-
-# git clone  https://github.com/gdy666/luci-app-lucky.git package/lucky
-# git clone https://github.com/sbwml/openwrt-alist.git package/openwrt-alist
 git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/luci-app-unblockneteasemusic
+git clone https://github.com/ophub/luci-app-amlogic.git package/luci-app-amlogic
 
 # Update feeds
 ./scripts/feeds update -a
 
-# 修改 xxx 为默认主题,可根据你喜欢的修改成其他的（不选择那些会自动改变为默认主题的主题才有效果）
-# sed -i 's/luci-theme-bootstrap/luci-theme-xxx/g' feeds/luci/collections/luci/Makefile
-
 # 删除包
-# rm -rf feeds/luci/themes/luci-theme-argon
 rm -rf feeds/packages/multimedia/UnblockNeteaseMusic
 rm -rf feeds/luci/applications/luci-app-unblockmusic
 rm -rf feeds/packages/multimedia/UnblockNeteaseMusic-Go
-
-# luci-app-argon-config
-# git clone https://github.com/jerrykuku/luci-app-argon-config.git package/luci-app-argon-config
-sed -i 's/system/services/g'  feeds/luci/applications/luci-app-argon-config/luasrc/controller/argon-config.lua
-
-# luci-app-design-config
-sed -i 's/system/services/g'  feeds/luci/applications/luci-app-design-config/luasrc/controller/*.lua
-
-# 调整VPN服务到VPN菜单
-# v2ray服务
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/controller/*.lua
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/model/cbi/v2ray_server/*.lua
-sed -i 's/services/vpn/g' feeds/luci/applications/luci-app-v2ray-server/luasrc/view/v2ray_server/*.htm
-# wireguard
-sed -i 's/status/vpn/g' feeds/luci/applications/luci-app-wireguard/luasrc/controller/wireguard.lua
-sed -i 's/92/2/g' feeds/luci/applications/luci-app-wireguard/luasrc/controller/wireguard.lua
-
-# 调整阿里云盘到存储菜单
-sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/controller/*.lua
-sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/model/cbi/aliyundrive-webdav/*.lua
-sed -i 's/services/nas/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/luasrc/view/aliyundrive-webdav/*.htm
 
 # 调整upnp到网络菜单
 sed -i 's/services/network/g' feeds/luci/applications/luci-app-upnp/luasrc/controller/*.lua
 sed -i 's/services/network/g' feeds/luci/applications/luci-app-upnp/luasrc/model/cbi/upnp/*.lua
 sed -i 's/services/network/g' feeds/luci/applications/luci-app-upnp/luasrc/view/*.htm
-
-# 修改插件名字
-sed -i 's/"阿里云盘 WebDAV"/"阿里云盘"/g' package/luci-app-aliyundrive-webdav/luci-app-aliyundrive-webdav/po/zh-cn/aliyundrive-webdav.po
-sed -i 's/WireGuard 状态/WireGuard/g' feeds/luci/applications/luci-app-wireguard/po/zh-cn/wireguard.po
 
 ./scripts/feeds install -a
 
